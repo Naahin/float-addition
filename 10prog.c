cf36d2d09f928134e348d4d25ffd7cd2ea51dda5
Naahin <98544401+Naahin@users.noreply.github.com>
2022-03-28 22:19:20 +0600

    Add files via upload

---
+++ 10prog.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter value for a: ");
+    scanf("%f",&a);
+    printf("\nEnter value for b: ");
+    scanf("%f",&b);
+    c=a+b;
+    printf("\n%f",c);
+
+
+}
